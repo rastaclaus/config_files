4d5030262bd9da0c1b6a237856826cd48276ede8
Artem Shestopalov <tema.rastaclaus@gmail.com>
2021-04-01 22:57:28 +0300

    deepl translate

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,15 +1,6 @@
-Front.registerInlineQuery({
-	url: `https://translate.yandex.net/api/v1.5/tr.json/translate
-				?key=trnsl.1.1.20171231T100146Z.4b564db549f21328.24eefc55e8411dd6a39d453dcec3d14eddd2844b
-				&lang=ru
-				&text=`,
-	parseResult: function(res) {
-		try {
-			res = JSON.parse(res.text);
-			return res.text;
-		} catch (e) {
-			return e;
-		}
-	}
+addSearchAliasX('l', 'deepl', 'https://www.deepl.com/translator#en/ru/', 's', 'https://www.deepl.com/translator#en/ru/', function(response) {
+    var res = JSON.parse(response.text);
+    return res.map(function(r){
+        return r.phrase;
+    });
 });
-

547e741810f7eaf673b84c1c9f6fac6e9d6e7d63
rastaclaus <tema.rastaclaus@gmail.com>
2022-01-09 18:45:53 +0300

    Update surfingkeys.js

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,4 +1,4 @@
-addSearchAliasX('l', 'deepl', 'https://www.deepl.com/translator#en/ru/', 's', 'https://www.deepl.com/translator#en/ru/', function(response) {
+addSearchAlias('l', 'deepl', 'https://www.deepl.com/translator#en/ru/', 's', 'https://www.deepl.com/translator#en/ru/', function(response) {
     var res = JSON.parse(response.text);
     return res.map(function(r){
         return r.phrase;

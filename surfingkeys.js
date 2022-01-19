53d22054b8ff8da97ab754cf1f8e7016e9317b53
Artem Shestopalov <tema.rastaclaus@gmail.com>
2022-01-19 17:36:53 +0300

    update surfingkeys config

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,25 +1,4 @@
-const {
-    aceVimMap,
-    mapkey,
-    imap,
-    imapkey,
-    getClickableElements,
-    vmapkey,
-    map,
-    unmap,
-    cmap,
-    addSearchAlias,
-    removeSearchAlias,
-    tabOpenLink,
-    readText,
-    Clipboard,
-    Front,
-    Hints,
-    Visual,
-    RUNTIME
-} = api;
-
-addSearchAlias('l', 'deepl', 'https://www.deepl.com/translator#en/ru/', 's', 'https://www.deepl.com/translator#en/ru/', function(response) {
+addSearchAliasX('l', 'deepl', 'https://www.deepl.com/translator#en/ru/', 's', 'https://www.deepl.com/translator#en/ru/', function(response) {
     var res = JSON.parse(response.text);
     return res.map(function(r){
         return r.phrase;

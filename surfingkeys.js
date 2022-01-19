115cd5f635c0924f3cad4e0eb9554227587e8f35
Artem Shestopalov <tema.rastaclaus@gmail.com>
2022-01-19 17:42:07 +0300

    revert surfingkeys config

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,4 +1,25 @@
-addSearchAliasX('l', 'deepl', 'https://www.deepl.com/translator#en/ru/', 's', 'https://www.deepl.com/translator#en/ru/', function(response) {
+const {
+    aceVimMap,
+    mapkey,
+    imap,
+    imapkey,
+    getClickableElements,
+    vmapkey,
+    map,
+    unmap,
+    cmap,
+    addSearchAlias,
+    removeSearchAlias,
+    tabOpenLink,
+    readText,
+    Clipboard,
+    Front,
+    Hints,
+    Visual,
+    RUNTIME
+} = api;
+
+addSearchAlias('l', 'deepl', 'https://www.deepl.com/translator#en/ru/', 's', 'https://www.deepl.com/translator#en/ru/', function(response) {
     var res = JSON.parse(response.text);
     return res.map(function(r){
         return r.phrase;

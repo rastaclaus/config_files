e205eeb729770d1fd8c8fbaab1ac48f2cd183c72
Artem Shestopalov <tema.rastaclaus@gmail.com>
2023-01-05 00:35:04 +0300

    add json pretier

--- nvim/lua/plugins-settings.lua
+++ nvim/lua/plugins-settings.lua
@@ -146,6 +146,7 @@ require("neo-tree").setup(
 vim.g.neoformat_enabled_python = {"black", "isort"}
 vim.g.neoformat_enabled_sql = {"pg_format"}
 vim.g.neoformat_enabled_yaml = {"prettier"}
+vim.g.neoformat_enabled_json = {"prettier"}
 vim.g.neoformat_run_all_formatters = 1
 
 require("lint").linters.pylint = require("pylint")

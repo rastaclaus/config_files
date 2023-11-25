cf6fcd46e18c8bd3a1360070bc4e854b5314a3b0
Artem Shestopalov <tema.rastaclaus@gmail.com>
2023-11-25 04:50:41 +0300

    darkman settings

--- light-mode.d/gtk-theme.sh
+++ light-mode.d/gtk-theme.sh
@@ -2,3 +2,4 @@
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 cp ~/.local/share/light-mode.d/settings.ini ~/.config/gtk-3.0
+cp ~/.local/share/light-mode.d/settings.ini ~/.config/gtk-4.0

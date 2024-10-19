bd1f1ed89eed447c8855609beb69412986b989b6
Rastaclaus <tema.rastaclaus@gmail.com>
2024-10-19 22:51:31 +0300

    Update surfingkeys.js

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,27 +1,9 @@
-const {
-    aceVimMap,
-    mapkey,
-    imap,
-    imapkey,
-    getClickableElements,
-    vmapkey,
-    map,
-    unmap,
-    cmap,
-    addSearchAlias,
-    removeSearchAlias,
-    tabOpenLink,
-    readText,
-    Clipboard,
-    Front,
-    Hints,
-    Visual,
-    RUNTIME
-} = api;
-
-addSearchAlias('l', 'deepl', 'https://www.deepl.com/translator#en/ru/', 's', 'https://www.deepl.com/translator#en/ru/', function(response) {
-    var res = JSON.parse(response.text);
-    return res.map(function(r){
-        return r.phrase;
-    });
+map('sl', 'Translate selected text with DeepL', function() {
+    var selectedText = window.getSelection().toString();
+    if (selectedText) {
+        var deeplUrl = `https://www.deepl.com/translator#en/ru/${encodeURIComponent(selectedText)}`;
+        window.open(deeplUrl, '_blank');
+    } else {
+        Front.showBanner('Please select text to translate');
+    }
 });

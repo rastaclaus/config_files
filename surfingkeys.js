0f1020c893b51439e6917588a6639c81451a4564
Rastaclaus <tema.rastaclaus@gmail.com>
2024-10-19 23:36:36 +0300

    Update surfingkeys.js

--- surfingkeys.js
+++ surfingkeys.js
@@ -19,12 +19,9 @@ const {
     RUNTIME
 } = api;
 
-api.map('sl', 'Translate selected text with DeepL', function() {
-    var selectedText = window.getSelection().toString();
-    if (selectedText) {
-        var deeplUrl = `https://www.deepl.com/translator#en/ru/${encodeURIComponent(selectedText)}`;
-        window.open(deeplUrl, '_blank');
-    } else {
-        Front.showBanner('Please select text to translate');
-    }
+addSearchAlias('l', 'deepl', 'https://duckduckgo.com/?q=', 's', function(response) {
+    var res = JSON.parse(response.text);
+    return res.map(function(r){
+        return r.phrase;
+    });
 });

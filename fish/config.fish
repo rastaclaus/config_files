b62b78196c6cde5adda350e2d613e1a956a85451
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-04 14:54:02 +0300

    set LLM tokens from config files

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
 pyenv init  - fish | source
 direnv hook fish | source
+
+set -x BOTHUB_API_KEY (cat $HOME/.config/openai_key)
+set -x OPENAI_API_KEY (cat $HOME/.config/openai_key)
+set -x NTECH_API_KEY (cat $HOME/.config/ntech_key)

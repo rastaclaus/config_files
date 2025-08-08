1f30beb043c57c128ec058f596091729967dd7f8
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-08 23:13:36 +0300

    fish alacritty aider models, alacritty as default terminal emulator

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    if [ -z $DISPLAY ] && [ "$(tty)" = /dev/tty1 ]
+        then
+        exec sway
+    end
+end
+pyenv init - fish | source
+direnv hook fish | source
+
+set -x BOTHUB_API_KEY (cat $HOME/.config/openai_key)
+set -x OPENAI_API_KEY (cat $HOME/.config/openai_key)
+set -x NTECH_API_KEY (cat $HOME/.config/ntech_key)

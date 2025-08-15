b4d36fcba11c0d7adb833a7f0ede0eafe1c223e9
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-15 20:22:19 +0300

    update colors

--- dark-mode.d/terminal-theme.sh
+++ dark-mode.d/terminal-theme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i 's/medium_light/hard_dark/' ~/.config/alacritty/alacritty.toml
+sed -i 's/bamboo_light/bamboo/' ~/.config/alacritty/alacritty.toml
 sed -i 's/opacity = .*/opacity = 0.9/' ~/.config/alacritty/alacritty.toml
 ~/.cargo/bin/nvim-ctrl 'TransparentEnable'
 ~/.cargo/bin/nvim-ctrl 'set background=dark'
--- light-mode.d/terminal-theme.sh
+++ light-mode.d/terminal-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sed -i 's/hard_dark/medium_light/' ~/.config/alacritty/alacritty.toml
+sed -i 's/bamboo/bamboo_light/' ~/.config/alacritty/alacritty.toml
 sed -i 's/opacity = .*/opacity = 1.0/' ~/.config/alacritty/alacritty.toml
 ~/.cargo/bin/nvim-ctrl 'TransparentDisable'
 ~/.cargo/bin/nvim-ctrl 'set background=light'

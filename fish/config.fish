9e04c4e5fa286ed1fc6b34c4478c0c48707b5dbd
Artem Shestopalov <a.shestopalov@ntechlab.com>
2025-08-19 00:58:45 +0300

    update light-dark-modes

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,20 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    if [ -z $DISPLAY ] && [ "$(tty)" = /dev/tty1 ]
+        then
+        exec sway
+    end
+
 end
-pyenv init  - fish | source
+
+if command -q darkman
+    if test (darkman get) = "light"
+        fish_config theme choose "Snow Day"
+    else
+        fish_config theme choose "Base16 Default Dark"
+    end
+end
+
+pyenv init - fish | source
 direnv hook fish | source
 
 set -x BOTHUB_API_KEY (cat $HOME/.config/openai_key)

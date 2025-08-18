9f7e2ce7d39cc4262ba286d164d67a1a74ead612
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-19 00:20:22 +0300

    dark/light mode choose

--- dark-mode.d/terminal-theme.sh
+++ dark-mode.d/terminal-theme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i 's/bamboo_light/bamboo/' ~/.config/alacritty/alacritty.toml
+sed -i 's/themes\/bamboo_light\.toml/themes\/bamboo.toml/' ~/.config/alacritty/alacritty.toml
 sed -i 's/opacity = .*/opacity = 0.8/' ~/.config/alacritty/alacritty.toml
 ~/.cargo/bin/nvim-ctrl 'TransparentEnable'
 ~/.cargo/bin/nvim-ctrl 'set background=dark'

d67736a9fe8ee6845a14d574f26b6b2678bdb72c
Artem Shestopalov <a.shestopalov@ntechlab.com>
2025-08-20 14:53:14 +0300

    fish settings

--- fish/config.fish
+++ fish/config.fish
@@ -9,6 +9,8 @@ end
 pyenv init - fish | source
 direnv hook fish | source
 
+fish_vi_key_bindings
+
 set -x BOTHUB_API_KEY (cat $HOME/.config/openai_key)
 set -x OPENAI_API_KEY (cat $HOME/.config/openai_key)
 set -x NTECH_API_KEY (cat $HOME/.config/ntech_key)

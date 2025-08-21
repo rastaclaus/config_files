0547e09c4217633dbefd355cfe6f49c873eb9080
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-21 03:28:49 +0300

    fish color switch

---
+++ fish/conf.d/update_theme.fish
@@ -0,0 +1,7 @@
+function update_theme --on-variable background_mode
+  if [ "$background_mode" = "dark" ]
+    fish_config theme choose 'ayu Dark'
+  else if [ "$background_mode" = "light" ]
+    fish_config theme choose "Snow Day"
+  end
+end

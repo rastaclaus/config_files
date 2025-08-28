b74e3b486f1a22f9be9256e499c03d98f10a7a92
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-28 14:30:05 +0300

    switch themes for light/dark mode for helix

--- dark-mode.d/terminal-theme.sh
+++ dark-mode.d/terminal-theme.sh
@@ -2,5 +2,6 @@
 
 sed -i 's/\/themes\/bamboo_light.toml/\/themes\/bamboo.toml/' ~/.config/alacritty/alacritty.toml
 sed -i 's/opacity = .*/opacity = 0.8/' ~/.config/alacritty/alacritty.toml
+sed -i 's/theme = "light"/theme = "dark"/' ~/.config/helix/config.toml
 ~/.cargo/bin/nvim-ctrl 'TransparentEnable'
 ~/.cargo/bin/nvim-ctrl 'set background=dark'

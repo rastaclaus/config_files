81493f549cbdfc364e658b9c275b467c41e03716
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-08-29 18:22:36 +0300

    disable transparency in neovim

--- light-mode.d/terminal-theme.sh
+++ light-mode.d/terminal-theme.sh
@@ -2,5 +2,4 @@
 sed -i 's/\/themes\/bamboo.toml/\/themes\/bamboo_light.toml/' ~/.config/alacritty/alacritty.toml
 sed -i 's/opacity = .*/opacity = 1.0/' ~/.config/alacritty/alacritty.toml
 sed -i 's/theme = "dark"/theme = "light"/' ~/.config/helix/config.toml
-~/.cargo/bin/nvim-ctrl 'TransparentDisable'
 ~/.cargo/bin/nvim-ctrl 'set background=light'

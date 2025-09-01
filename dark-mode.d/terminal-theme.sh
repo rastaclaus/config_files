4d346a796bc85e122f15c33479ac285da6882b9a
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-09-01 18:11:38 +0300

    sync helix theme toggle

--- dark-mode.d/terminal-theme.sh
+++ dark-mode.d/terminal-theme.sh
@@ -2,5 +2,5 @@
 
 sed -i 's/\/themes\/bamboo_light.toml/\/themes\/bamboo.toml/' ~/.config/alacritty/alacritty.toml
 sed -i 's/opacity = .*/opacity = 0.8/' ~/.config/alacritty/alacritty.toml
-~/.cargo/bin/nvim-ctrl 'TransparentEnable'
+sed -i 's/theme = "light"/theme = "dark"/' ~/.config/helix/config.toml
 ~/.cargo/bin/nvim-ctrl 'set background=dark'
--- light-mode.d/terminal-theme.sh
+++ light-mode.d/terminal-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sed -i 's/\/themes\/bamboo.toml/\/themes\/bamboo_light.toml/' ~/.config/alacritty/alacritty.toml
 sed -i 's/opacity = .*/opacity = 1.0/' ~/.config/alacritty/alacritty.toml
-~/.cargo/bin/nvim-ctrl 'TransparentDisable'
+sed -i 's/theme = "dark"/theme = "light"/' ~/.config/helix/config.toml
 ~/.cargo/bin/nvim-ctrl 'set background=light'

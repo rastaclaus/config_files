1a664162850def716d729ef97713a6d2493b87a7
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-09-02 17:52:09 +0300

    update config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,3 @@
-if status is-interactive
-    if [ -z $DISPLAY ] && [ "$(tty)" = /dev/tty1 ]
-        then
-        exec sway
-    end
-end
-
 if command -q darkman
     if test (darkman get) = "light"
         fish_config theme choose "Snow Day"
@@ -19,3 +12,10 @@ direnv hook fish | source
 set -x BOTHUB_API_KEY (cat $HOME/.config/openai_key)
 set -x OPENAI_API_KEY (cat $HOME/.config/openai_key)
 set -x NTECH_API_KEY (cat $HOME/.config/ntech_key)
+
+if status is-interactive
+    if [ -z $DISPLAY ] && [ "$(tty)" = /dev/tty1 ]
+        exec sway
+    end
+end
+

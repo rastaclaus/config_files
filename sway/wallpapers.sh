204498df5633a782f739cd26742da4a0ce00efaf
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-09-30 22:38:03 +0300

    wallpapers for laptop mon

--- sway/wallpapers.sh
+++ sway/wallpapers.sh
@@ -11,16 +11,15 @@ IMAGE_PATH="$WALLPAPER_DIR/$IMAGE"
 
 # Получаем размеры мониторов
 MONITOR1=$(swaymsg -t get_outputs | jq -r '.[] | select(.active) | .name' | sed -n '1p')
-MONITOR2=$(swaymsg -t get_outputs | jq -r '.[] | select(.active) | .name' | sed -n '2p')
+# MONITOR2=$(swaymsg -t get_outputs | jq -r '.[] | select(.active) | .name' | sed -n '2p')
 
-echo $MONITOR1
-echo $MONITOR2
+# echo $MONITOR1
+# echo $MONITOR2
 
 # Разделяем изображение на две части
-magick "$IMAGE_PATH" -crop 50%x100% +repage /tmp/%d_$IMAGE
+# magick "$IMAGE_PATH" -crop 50%x100% +repage /tmp/%d_$IMAGE
 
-ls -alh /tmp/*.jpg
+# ls -alh /tmp/*.jpg
 
 # Устанавливаем обои
-swaybg -o "$MONITOR1" -i "/tmp/0_$IMAGE" --mode fill &
-swaybg -o "$MONITOR2" -i "/tmp/1_$IMAGE" --mode fill &
+swaybg -o "$MONITOR1" -i $IMAGE_PATH --mode fill & 
